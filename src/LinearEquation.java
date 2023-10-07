634e9c65d9a1d88edddd05788058c379458dcd45
Veronica Joseph <lalalal@gmail.com>
2023-10-06 23:50:43 -0400

    - made my instance variables integers - fixed the variables in my constructor - made methods static to use in my toString() method - fixed the variables in my toString() method

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,32 +1,38 @@
 public class LinearEquation {
-    private double firstX;
-    private double firstY;
-    private double secondX;
-    private double secondY;
-    private double newX;
+    private int firstX;
+    private int firstY;
+    private int secondX;
+    private int secondY;
+    private int newX;
 
-    public LinearEquation(double firstX, double firstY, double secondX, double secondY) {
-        double x1 = firstX;
-        double x2 = secondX;
-        double y1 = firstY;
-        double y2 = secondY;
+
+    public LinearEquation(int x1, int y1, int x2, int y2) {
+        firstX = x1;
+        firstY = y1;
+        secondX = x2;
+        secondY = y2;
     }
 
-    public String slopeAsFraction(double x1, double x2, double y1, double y2) {
+    // returns the slope as a fraction
+    public static String slopeAsFraction(double x1, double y1, double x2, double y2) {
         double num = y2 - y1;
         double denom = x2 - x1;
         return num + "/" + denom;
     }
 
-    public double distance(double x1, double x2, double y1, double y2) {
-        return Math.sqrt((y2-y1) * (y2-y1) + (x2-x1) * (x2-x1));
+    // returns the distance between the points
+    public static double distance(double x1, double y1, double x2, double y2) {
+        double calcDistance = Math.sqrt((y2-y1) * (y2-y1) + (x2-x1) * (x2-x1));
+        return Math.round(calcDistance * 100) / 100.0;
     }
 
-        public double getYIntercept(double x1, double y1, double x2, double y2) {
+    // gets the y intercept of the line
+    public static double getYIntercept(double x1, double y1, double x2, double y2) {
         double slope = (y2 - y1) / (x2 - x1);
         return y1 - (slope * x1);
     }
 
+    // solves the value for the y coord after receiving an x coord from the user
     public String solvedYCoordinate(double newX, double yIntercept, double x1, double y1, double x2, double y2) {
         double slope = (y2 - y1) / (x2 - x1);
         double x = newX * slope;
@@ -34,14 +40,19 @@ public class LinearEquation {
         return "(" + newX + "," + solvedYCoord + ")";
     }
 
-//    public String toString() {
-//        String str = "First pair: (" + firstX + "," + firstY + ")" + '\n' +
-//                "Second pair: (" + secondX + "," + ")" + '\n' +
-//                "Slope of line: " + slope + '\n' + "Y-intercept: " + yIntercept + '\n'
-//                + "Slope intercept form: " + "y = " + slope + "x + " + yIntercept + '\n'
-//                + "Y-intercept: " + yIntercept + '\n' + "Distance between points: " + distance;
-//        return str;
-//    }
+    // prints out all required information
+    public String toString() {
+        String slope = LinearEquation.slopeAsFraction(firstX, firstY, secondX, secondY);
+        double slope2 = (double) (secondY - firstY) / (secondX - firstX);
+        double distance = LinearEquation.distance(firstX, firstY, secondX, secondY);
+        double yIntercept = LinearEquation.getYIntercept(firstX, firstY, secondX, secondY);
+        String str = "First pair: (" + firstX + "," + firstY + ")" + '\n' +
+                "Second pair: (" + secondX + "," + secondY + ")" + '\n' +
+                "Slope of line: " + slope2 + '\n' + "Y-intercept: " + yIntercept + '\n'
+                + "Slope intercept form: " + "y = " + slope + "x + " + yIntercept + '\n'
+                + "Distance between points: " + distance;
+        return str;
+    }
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        System.out.print("Enter your first coordinate point: ");
-        s.nextLine();
-        System.out.print("Enter your second coordinate point: ");
-        s.nextLine();
-
-
 
     }
 }
