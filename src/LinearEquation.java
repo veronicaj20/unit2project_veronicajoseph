063a5565665e48bd698f77174b58e96e9a18f617
veronicajoseph <lalaland@gmail.com>
2023-10-09 20:08:49 -0400

    - finished the main program - added a slope method to be used inside other methods - modified the toString and solvedYCoordinate methods - aligned my design document with my program

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -3,8 +3,6 @@ public class LinearEquation {
     private int firstY;
     private int secondX;
     private int secondY;
-    private int newX;
-
 
     public LinearEquation(int x1, int y1, int x2, int y2) {
         firstX = x1;
@@ -14,42 +12,47 @@ public class LinearEquation {
     }
 
     // returns the slope as a fraction
-    public static String slopeAsFraction(double x1, double y1, double x2, double y2) {
+    public String slopeAsFraction(double x1, double y1, double x2, double y2) {
         double num = y2 - y1;
         double denom = x2 - x1;
         return num + "/" + denom;
     }
 
+    // returns the slope as a double
+    public double slope(double x1, double y1, double x2, double y2) {
+        double slope = (y2 - y1) / (x2 - x1);
+        return (double) Math.round(slope * 100) / 100;
+    }
+
     // returns the distance between the points
-    public static double distance(double x1, double y1, double x2, double y2) {
+    public double distance(double x1, double y1, double x2, double y2) {
         double calcDistance = Math.sqrt((y2-y1) * (y2-y1) + (x2-x1) * (x2-x1));
-        return Math.round(calcDistance * 100) / 100.0;
+        return (double) Math.round(calcDistance * 100) / 100;
     }
 
     // gets the y intercept of the line
-    public static double getYIntercept(double x1, double y1, double x2, double y2) {
-        double slope = (y2 - y1) / (x2 - x1);
+    public double getYIntercept(double x1, double y1, double x2, double y2) {
+        double slope = slope(x1, y1, x2, y2);
         return y1 - (slope * x1);
     }
 
-    // solves the value for the y coord after receiving an x coord from the user
-    public String solvedYCoordinate(double newX, double yIntercept, double x1, double y1, double x2, double y2) {
-        double slope = (y2 - y1) / (x2 - x1);
-        double x = newX * slope;
-        double solvedYCoord = x + yIntercept;
-        return "(" + newX + "," + solvedYCoord + ")";
+    // solves the value for the y after receiving an x value from the user
+    public double solvedYCoordinate(double newX, double yIntercept, double x1, double y1, double x2, double y2) {
+        double slope = slope(x1, y1, x2, y2);
+        double slopeTimesInput = newX * slope;
+        return (double) Math.round((slopeTimesInput + yIntercept) * 100) / 100;
     }
 
     // prints out all required information
     public String toString() {
-        String slope = LinearEquation.slopeAsFraction(firstX, firstY, secondX, secondY);
-        double slope2 = (double) (secondY - firstY) / (secondX - firstX);
-        double distance = LinearEquation.distance(firstX, firstY, secondX, secondY);
-        double yIntercept = LinearEquation.getYIntercept(firstX, firstY, secondX, secondY);
+        double slope = slope(firstX, firstY, secondX, secondY);
+        String slope2 = slopeAsFraction(firstX, firstY, secondX, secondY);
+        double distance = distance(firstX, firstY, secondX, secondY);
+        double yIntercept = getYIntercept(firstX, firstY, secondX, secondY);
         String str = "First pair: (" + firstX + "," + firstY + ")" + '\n' +
                 "Second pair: (" + secondX + "," + secondY + ")" + '\n' +
-                "Slope of line: " + slope2 + '\n' + "Y-intercept: " + yIntercept + '\n'
-                + "Slope intercept form: " + "y = " + slope + "x + " + yIntercept + '\n'
+                "Slope of line: " + slope + '\n' + "Y-intercept: " + yIntercept + '\n'
+                + "Slope intercept form: " + "y = " + slope2 + "x + " + yIntercept + '\n'
                 + "Distance between points: " + distance;
         return str;
     }
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,38 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
+        System.out.print("Enter your first coordinate point: ");
+        String firstPoint = s.nextLine();
+        System.out.print("Enter your second coordinate point: ");
+        String secondPoint = s.nextLine();
+        System.out.println();
+
+        // gets integer values without parentheses and commas
+        int commaIndex = firstPoint.indexOf(","); // retrieves the index of the commas
+        int commaIndex2 = secondPoint.indexOf(",");
+        int x1 = Integer.parseInt(firstPoint.substring(1, commaIndex));
+        int y1 = Integer.parseInt(firstPoint.substring(commaIndex + 1, firstPoint.length() - 1));
+        int x2 = Integer.parseInt(secondPoint.substring(1, commaIndex2));
+        int y2 = Integer.parseInt(secondPoint.substring(commaIndex2 + 1, secondPoint.length() - 1));
+
+        // creates an object and prints out required information
+        LinearEquation points = new LinearEquation(x1, y1, x2, y2);
+        System.out.println(points);
+
+        // asks user for third x value to solve for respective y value
+        System.out.print("Enter a third x-value: ");
+        String inputtedX = s.nextLine();
+        double newXVal = Double.parseDouble(inputtedX);
+        double yInt = points.getYIntercept(x1, y1, x2, y2);
+        double newYVal = points.solvedYCoordinate(newXVal, yInt, x1, y1, x2, y2);
+        System.out.print("The solved coordinate point is: (" + newXVal + "," + newYVal + ")");
+
+
+
+
+        
+
+
 
     }
 }
